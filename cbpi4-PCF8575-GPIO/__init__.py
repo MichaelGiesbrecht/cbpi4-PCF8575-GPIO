c082de2e059b7b555d3f60259e38b664bb157319
Michael Giesbrecht <michael_giesbrecht@outlook.com>
2024-08-18 15:47:20 -0700

    move library to init

--- cbpi4-PCF8575-GPIO/__init__.py
+++ cbpi4-PCF8575-GPIO/__init__.py
@@ -6,7 +6,7 @@ import logging
 from unittest.mock import MagicMock, patch
 import asyncio
 import random
-from cbpi4-PCF8575-GPIO import pcf8575_io 
+import PCF85
 from cbpi.api import *
 from cbpi.api.config import ConfigType
 from cbpi.api.dataclasses import Props
@@ -159,3 +159,38 @@ def setup(cbpi):
     cbpi.plugin.register("PCF8575Actor", PCF8575Actor)
     cbpi.plugin.register("PCF8575_Config",PCF8575)
     pass
+
+
+
+class PCF:
+
+    def __init__(self, address):
+        self.address = address
+        self.status = True
+        self.pinModeFlag = 0x00
+        self.smBusNum = 1
+        PCF85.setup(address, self.smBusNum, self.status)
+
+    def pin_mode(self, PinName, Mode):
+        self.pinModeFlag = PCF85.pin_mode(PinName, Mode, self.pinModeFlag)
+
+    def read(self, PinName):
+        return PCF85.digitalRead(PinName, self.smBusNum, self.address)
+
+    def write(self, PinName, Val):
+        PCF85.digitalWrite(PinName, Val, self.address, self.pinModeFlag, self.smBusNum)
+
+    def set_i2cBus(self, port):
+        self.smBusNum = port
+    
+    def get_i2cBus(self):
+        return self.smBusNum
+    
+    def get_pin_mode(self, PinName):
+        return PCF85.get_pin_mode(PinName,self.pinModeFlag)
+        
+    def is_pin_output(self, PinName):
+        return PCF85.is_pin_output(PinName,self.pinModeFlag)
+    
+    def get_all_mode(self):
+        return PCF85.get_all_mode(self.pinModeFlag)
